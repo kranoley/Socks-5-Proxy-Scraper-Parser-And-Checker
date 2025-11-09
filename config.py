60aedb709c82efdc2f03c6e48c4fc360f1c9dde7
kranoley <127663416+kranoley@users.noreply.github.com>
2025-11-09 20:01:44 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
 PROXY_SOURCES = [
-    'https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks5',
-    'https://www.proxy-list.download/api/v1/get?type=socks5',
-    'https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/socks5.txt',
-    'https://raw.githubusercontent.com/hookzof/socks5_list/master/proxy.txt',
-    'https://raw.githubusercontent.com/mmpx12/proxy-list/master/socks5.txt',
+    'https://sockslist.us/Raw',
+    'https://proxymania.su/free-proxy?type=SOCKS5',
+    'https://proxyverity.com/proxies-by-types/socks5/',
+    'https://www.freeproxy.world/?type=socks5',
+    'https://raw.githubusercontent.com/hookzof/socks5_list/refs/heads/master/proxy.txt',
+    "https://raw.githubusercontent.com/proxifly/free-proxy-list/refs/heads/main/proxies/protocols/socks5/data.txt",
+    "https://raw.githubusercontent.com/watchttvv/free-proxy-list/refs/heads/main/proxy.txt",
+    "https://raw.githubusercontent.com/databay-labs/free-proxy-list/refs/heads/master/socks5.txt",
+    "https://raw.githubusercontent.com/dpangestuw/Free-Proxy/refs/heads/main/socks5_proxies.txt",
 ]
 TEST_URLS = [
     'http://httpbin.org/ip',
